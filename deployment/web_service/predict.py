888ac0d9acb58314c617a349c580d6465e5d4c03
Almudena <almudena.zhou.rl@gmail.com>
2024-07-17 20:24:45 +0200

    Added TEST_RUN

--- deployment/web_service/predict.py
+++ deployment/web_service/predict.py
@@ -2,21 +2,22 @@ import os
 import sys
 sys.path.append(os.getcwd())
 
-from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 
 from scripts import load_models, predict
 
 
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", None)
-print(MLFLOW_TRACKING_URI)
+TEST_RUN = os.getenv("TEST_RUN", "False") == "True"
 
-load_dotenv()
+if TEST_RUN:
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", None)
+    print(MLFLOW_TRACKING_URI)
 
-if MLFLOW_TRACKING_URI is not None: 
-    os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI
+    from dotenv import load_dotenv
+    load_dotenv()
 
-print(MLFLOW_TRACKING_URI)
+    if MLFLOW_TRACKING_URI is not None: 
+        os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI
 
 RUN_ID = os.getenv("RUN_ID")
 EXPERIMENT_NAME = os.getenv("EXPERIMENT_NAME")
