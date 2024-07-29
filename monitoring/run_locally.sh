130ecb54738b03c0be14300737245cf5c6217d86
Almudena <almudena.zhou.rl@gmail.com>
2024-07-30 00:30:35 +0200

    Fixed wrong path to run adapt_data.py drom run_locally.sh

--- monitoring/run_locally.sh
+++ monitoring/run_locally.sh
@@ -11,6 +11,6 @@ export TEST_RUN="True"
 set -e
 
 python monitoring/src/scripts/create_db.py
-python monitoring/src/scripts/adapt_data.py
+python monitoring/src/pipelines/adapt_data.py
 python monitoring/src/pipelines/prepare_reference_data.py
 python monitoring/src/pipelines/scheduler.py
