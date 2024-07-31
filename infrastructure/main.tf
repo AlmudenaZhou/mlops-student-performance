1e51d10734046c01675d6034a581e0a0c7184a4e
Almudena <almudena.zhou.rl@gmail.com>
2024-07-31 10:58:37 +0200

    Fixed bucket location

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket  = "mlflow-artifact-student-performance"
     key     = "stg-terraform.tfstate"
-    region  = "eu-west-1"
+    region  = "us-east-1"
     encrypt = true
   }
 }
